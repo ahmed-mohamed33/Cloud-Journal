cbece173ceb27f8c9ebe93d2f288e7b2c5bf6fa2
ahmed-mohamed33 <a7medselim93@gmail.com>
2025-06-16 06:07:33 +0300

    - Refactored Navbar component for better readability and responsiveness, Postponed Projects page , Search,Notications

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import Home from '../components/Home';
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import BotCloud from '../components/BotCloud';
+import { Engineering } from '@mui/icons-material';
     
 const Projects = () => {
     return (
    <>
    <Home/>
+   <Box sx={{marginTop: '64px' ,backgroundColor: 'white' ,width: '100%', height: '50vh'}}>
    <Typography variant="h4" component="h1" align="center" gutterBottom>Coming Soon</Typography>
+   <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' ,gap: 1 ,color: 'black'}}>
+
+   <Engineering sx={{fontSize: '200px', color:'#a0d4ee'}}/>
+   </Box>
+   </Box>
    <BotCloud/>
    </>
     )
